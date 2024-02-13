0ccaf5f2e553a3a92b53152c52b058d8719beb9f
OsirisJr16 <juniorhero81@gmail.com>
2024-02-13 22:35:20 +0300

    Header Home

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,7 +1,8 @@
+import Main from "./pages/Main/Main";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello World</h1>
-    </main>
+    <>
+      <Main/>
+    </>
   );
 }
