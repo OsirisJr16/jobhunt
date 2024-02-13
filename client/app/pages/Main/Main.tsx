7a381a4210dddf91215ad76607863548161489d0
OsirisJr16 <juniorhero81@gmail.com>
2024-02-13 23:06:06 +0300

    header Home

--- client/app/pages/Main/Main.tsx
+++ client/app/pages/Main/Main.tsx
@@ -1,6 +1,7 @@
 'use client'
 import React , {useState} from 'react'
 import { Bars3Icon , XMarkIcon } from '@heroicons/react/16/solid'
+import { Dialog } from '@headlessui/react'
 
 const Main = () => {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false) ; 
@@ -41,6 +42,47 @@ const Main = () => {
                 </a>
             </div>
         </nav>
+        <Dialog as='div' className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}> 
+            <div className="fixed inset-0 z-50" />
+            <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10"> 
+                <div className="flex items-center justify-between">
+                    <a href="#" className="-m-1.5 p-1.5">
+                        <span className="sr-only">Your Company</span>
+                        <img
+                        className="h-8 w-auto"
+                        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                        alt=""
+                        />
+                    </a>
+                    <button
+                        type="button"
+                        className="-m-2.5 rounded-md p-2.5 text-gray-700"
+                        onClick={() => setMobileMenuOpen(false)}
+                    >
+                        <span className="sr-only">Close menu</span>
+                        <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                    </button>
+                </div>
+                <div className="mt-6 flow-root"> 
+                    <div className="-my-6 divide-y divide-gray-500/10"> 
+                        <div className='py-6'> 
+                            <a
+                                href="#"
+                                className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-blue-500 hover:bg-gray-50"
+                            >
+                                Log in
+                            </a>
+                            <a
+                                href="#"
+                                className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-500 hover:bg-gray-50"
+                            >
+                                Post a Job
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </Dialog.Panel>
+        </Dialog>
       </header>
     </div>
   )
