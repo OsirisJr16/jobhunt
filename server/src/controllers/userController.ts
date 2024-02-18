66f7f5fe651de69075687481c3e44cc8cd220032
OsirisJr16 <juniorhero81@gmail.com>
2024-02-18 23:51:52 +0300

    using sceret-key

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -4,30 +4,26 @@ import { User } from "../entity/user.entity";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import { myDataSource } from "../configs/app-data-source";
-
+require('dotenv').config();
+const SECRET_KEY = process.env.SECRET_KEY ; 
 export const login = async (req: Request, res: Response) => {
     const { email, password } = req.body;
-
     if (!email || !password) {
         return res.status(400).json({ message: "Email and password are required." });
     }
-
     try {
         const userRepository = myDataSource.getRepository(User);
         const user = await userRepository.findOne({ where: { email } });
-
         if (!user) {
             return res.status(401).json({ message: "Invalid email or password." });
         }
-
-        //tokony Crypté ny password raisiny  ; 
+        //tokony Crypté ny password raisiny miandry Client ; 
         // const isPasswordValid = await bcrypt.compare(password, user.password);
 
         // if (!isPasswordValid) {
         //     return res.status(401).json({ message: "Invalid email or password." });
         // }
-
-        const token = jwt.sign({ userId: user.id, email: user.email }, "key-secret", { expiresIn: "1h" });
+        const token = jwt.sign({ userId: user.id, email: user.email }, String(SECRET_KEY), { expiresIn: "1h" });
 
         res.status(200).json({ message:`Welcome ${user.username}` , token });
     } catch (error) {
