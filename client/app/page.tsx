185230e32b873feb4b2f64df99d3a854c4ce372a
OsirisJr16 <juniorhero81@gmail.com>
2024-02-22 22:32:10 +0300

    update UI

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,102 +1,126 @@
-'use client'
-import React , {useState} from 'react'
-import { Bars3Icon , XMarkIcon } from '@heroicons/react/16/solid'
-import { Dialog } from '@headlessui/react'
+"use client";
+import React, { useState } from "react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/16/solid";
+import { Dialog } from "@headlessui/react";
 
-import Link from 'next/link'
+import Link from "next/link";
 const Home = () => {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false) ;  
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
   return (
     <>
-    <div className='bg-white'>
-      <header className='absolute inset-x-0 top-0 z-50'> 
-        <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global"> 
+      <div className="bg-white">
+        <header className="absolute inset-x-0 top-0 z-50">
+          <nav
+            className="flex items-center justify-between p-6 lg:px-8"
+            aria-label="Global"
+          >
             <div className="flex lg:flex-1">
-                <a href="#" className="-m-1.5 p-1.5">
+              <a href="#" className="-m-1.5 p-1.5">
                 <span className="sr-only">Your Company</span>
                 <img
-                    className="h-8 w-auto"
-                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                    alt=""
+                  className="h-8 w-auto"
+                  src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                  alt=""
                 />
-                </a>
+              </a>
             </div>
             <div className="flex lg:hidden">
-                <button
+              <button
                 type="button"
                 className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
                 onClick={() => setMobileMenuOpen(true)}
-                >
+              >
                 <span className="sr-only">Open main menu</span>
                 <Bars3Icon className="h-6 w-6" aria-hidden="true" />
-                </button>
+              </button>
             </div>
-            <div className='hidden lg:flex lg:gap-x-12'></div>
+            <div className="hidden lg:flex lg:gap-x-12"></div>
             <div className="hidden lg:gap-x-8 lg:flex lg:flex-1 lg:justify-end">
-                <Link href="/auth/login?role=student" className="text-lg font-semibold leading-6 text-blue-500">
-                    Sign in  
-                </Link>
-                <a> 
-                    |
-                </a>
-                <a href='#' className="text-lg font-semibold leading-6 text-gray-900">
-                    Post a Job
-                </a>
+              <Link
+                href="/auth/login?role=student"
+                className="text-lg font-semibold leading-6 text-blue-500"
+              >
+                Sign in
+              </Link>
+              <a>|</a>
+              <a
+                href="#"
+                className="text-lg font-semibold leading-6 text-gray-900"
+              >
+                Post a Job
+              </a>
             </div>
-        </nav>
-        <Dialog as='div' className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}> 
+          </nav>
+          <Dialog
+            as="div"
+            className="lg:hidden"
+            open={mobileMenuOpen}
+            onClose={setMobileMenuOpen}
+          >
             <div className="fixed inset-0 z-50" />
-            <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10"> 
-                <div className="flex items-center justify-between">
-                    <a href="#" className="-m-1.5 p-1.5">
-                        <span className="sr-only">Your Company</span>
-                        <img
-                        className="h-8 w-auto"
-                        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                        alt=""
-                        />
+            <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+              <div className="flex items-center justify-between">
+                <a href="#" className="-m-1.5 p-1.5">
+                  <span className="sr-only">Your Company</span>
+                  <img
+                    className="h-8 w-auto"
+                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                    alt=""
+                  />
+                </a>
+                <button
+                  type="button"
+                  className="-m-2.5 rounded-md p-2.5 text-gray-700"
+                  onClick={() => setMobileMenuOpen(false)}
+                >
+                  <span className="sr-only">Close menu</span>
+                  <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                </button>
+              </div>
+              <div className="mt-6 flow-root">
+                <div className="-my-6 divide-y divide-gray-500/10">
+                  <div className="py-6">
+                    <a className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-blue-500 hover:bg-gray-50">
+                      Log in
                     </a>
-                    <button
-                        type="button"
-                        className="-m-2.5 rounded-md p-2.5 text-gray-700"
-                        onClick={() => setMobileMenuOpen(false)}
-                    >
-                        <span className="sr-only">Close menu</span>
-                        <XMarkIcon className="h-6 w-6" aria-hidden="true" />
-                    </button>
+                    <a className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-500 hover:bg-gray-50">
+                      Post a Job
+                    </a>
+                  </div>
                 </div>
-                <div className="mt-6 flow-root"> 
-                    <div className="-my-6 divide-y divide-gray-500/10"> 
-                        <div className='py-6'> 
-                            <a 
-                            
-                            className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-blue-500 hover:bg-gray-50">
-                                    Log in
-                            </a>
-                            <a
-                                className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-500 hover:bg-gray-50"
-                            >
-                                Post a Job
-                            </a>
-                        </div>
-                    </div>
-                </div>
+              </div>
             </Dialog.Panel>
-        </Dialog>
-      </header>
-    </div>
-    <div className="relative  lg:px-8">
-        <div className='mx-auto max-w-2xl py-32 sm:py-48 lg:py-56'> 
-            <div className="flex items-center bg-white rounded px-4 py-3">
-                <input type="text" placeholder="Job title..." className=" rounded flex-grow h-16 px-4 py-2 mr-2 text-sm focus:outline-none"/>
-                <input type="text" placeholder="Location" className="rounded h-16 px-4 py-2 mr-2 text-sm focus:outline-none"/>
-                <button className="bg-blue-500 text-white h-16 px-6 py-2 rounded hover:bg-blue-600 focus:outline-none">Find Jobs</button>
-            </div>
+          </Dialog>
+        </header>
+      </div>
+      <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
+        <div>
+          <p className="mx-auto max-w-2xl text-3xl font-bold tracking-tight text-gray-900 dark:text-gray-200 sm:text-4xl">
+            Didn't find component you were looking for?
+          </p>
+          <form>
+            <label className="mx-auto mt-8 relative bg-white min-w-sm max-w-2xl flex flex-col md:flex-row items-center justify-center border py-2 px-2 rounded-2xl gap-2  focus-within:border-gray-300">
+              <input
+                id="search-bar"
+                placeholder="Job title"
+                className="px-6 py-2 w-full rounded-md flex-1 outline-none bg-white"
+              />
+              <button
+                type="submit"
+                className="w-full md:w-auto px-6 py-3 bg-blue-400 text-white fill-white active:scale-95 duration-100 border will-change-transform overflow-hidden relative rounded-xl transition-all"
+              >
+                <div className="flex items-center transition-all opacity-1">
+                  <span className="text-sm font-semibold whitespace-nowrap truncate mx-auto">
+                    Search
+                  </span>
+                </div>
+              </button>
+            </label>
+          </form>
         </div>
-    </div>
-
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
