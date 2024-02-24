a6aba315a2719e1d6a387717ece89de00d339516
OsirisJr16 <juniorhero81@gmail.com>
2024-02-24 15:52:20 +0300

    register page

--- client/app/auth/register/page.tsx
+++ client/app/auth/register/page.tsx
@@ -1,11 +1,18 @@
+"use client"
 import React from 'react'
-
+import { useRouter , useSearchParams } from 'next/navigation'
 const page = () => {
+  const router = useRouter() ; 
+  const role = useSearchParams().get("role") ; 
+  
   return (
-    <div>
-      Register Page
+    <div className='min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12'>
+      <div className='relative py-3 sm:max-w-xl sm:mx-auto'> 
+       Register page {role}
+      </div>
     </div>
   )
 }
 
 export default page
+
