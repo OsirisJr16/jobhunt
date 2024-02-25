cc44aae2742a77988888da737dc3bd9080c9d770
OsirisJr16 <juniorhero81@gmail.com>
2024-02-25 23:22:35 +0300

    commit

--- client/app/auth/register/page.tsx
+++ client/app/auth/register/page.tsx
@@ -1,18 +1,34 @@
-"use client"
-import React from 'react'
-import { useRouter , useSearchParams } from 'next/navigation'
-const page = () => {
-  const router = useRouter() ; 
-  const role = useSearchParams().get("role") ; 
-  
+"use client";
+import React from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+const CompanyRegister = () => {
   return (
-    <div className='min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12'>
-      <div className='relative py-3 sm:max-w-xl sm:mx-auto'> 
-       Register page {role}
-      </div>
+    <>
+      <p>Company za</p>
+    </>
+  );
+};
+const StudentRegister = () => {
+  return (
+    <>
+      <p>Student za</p>
+    </>
+  );
+};
+const page = () => {
+  const router = useRouter();
+  const role = useSearchParams().get("role");
+  let registerPage: JSX.Element | null = null;
+  if (role === "company") {
+    registerPage = <CompanyRegister />;
+  } else {
+    registerPage = <StudentRegister />;
+  }
+  return (
+    <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
+      <div className="relative py-3 sm:max-w-xl sm:mx-auto">{registerPage}</div>
     </div>
-  )
-}
+  );
+};
 
-export default page
-
+export default page;
