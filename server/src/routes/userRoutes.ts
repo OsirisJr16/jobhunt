e7eea465e16f7bf7673775d93e1df612dfb12b30
OsirisJr16 <juniorhero81@gmail.com>
2024-03-01 23:43:23 +0300

    route

--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -1,8 +1,10 @@
 import express from "express" ; 
-import { login , profile } from "../controllers/userController";
+import { login , profile , registerCompany } from "../controllers/userController";
 import verifyToken from "../middlewares/authenticate";
 const authRouter = express.Router() 
 
 authRouter.post("/" , login) 
+authRouter.post("/registerCompany" , registerCompany)
 authRouter.get("/profile" , verifyToken , profile)
+
 export default authRouter ; 
