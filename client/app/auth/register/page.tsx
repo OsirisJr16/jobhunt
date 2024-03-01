8b6591f60fd0572638e170eb0485108bc3371b66
OsirisJr16 <juniorhero81@gmail.com>
2024-03-01 21:40:52 +0300

    register Page

--- client/app/auth/register/page.tsx
+++ client/app/auth/register/page.tsx
@@ -96,6 +96,17 @@ const CompanyRegister = () => {
               />
             </div>
           </div>
+          <div> 
+            <label
+              htmlFor="companyDescription"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            > 
+              Description
+            </label>
+            <div className="mt-2"> 
+              <textarea name="companyDescription" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
+            </div>
+          </div>
           <div>
             <button
               type="submit"
