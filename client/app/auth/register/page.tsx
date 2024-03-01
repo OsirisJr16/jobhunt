769c15b1c1bbd61dd39c3198fdf12333bd7555d5
OsirisJr16 <juniorhero81@gmail.com>
2024-03-01 13:49:34 +0300

    company register Page

--- client/app/auth/register/page.tsx
+++ client/app/auth/register/page.tsx
@@ -80,6 +80,23 @@ const CompanyRegister = () => {
             </div>
           </div>
           <div>
+            <label
+              htmlFor="companyAdress"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Company Adress
+            </label>
+            <div>
+              <input
+                id="companyAdress"
+                name="companyAdress"
+                required
+                type="text"
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+          <div>
             <button
               type="submit"
               className="flex w-full justify-center rounded-md bg-blue-400 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
