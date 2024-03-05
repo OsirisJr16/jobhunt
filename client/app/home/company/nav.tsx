3ebc814b498438444bcc2d49c42f205de00172a4
OsirisJr16 <juniorhero81@gmail.com>
2024-03-05 22:11:50 +0300

    file nav.tsx company

---
+++ client/app/home/company/nav.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const nav = () => {
+  const _navigation = [
+    {name : "Dashboard" , current: true , href:"#"},
+    {name : "Jobs" , current: false , href:"#"},
+    {name : "Application" , current: false , href:"#"},
+    {name : "profile" ,  current : false , href:"#"}
+  ]
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default nav
