30abe9167c1ae07d7cdbf24580ab1a7c53554c7e
OsirisJr16 <juniorhero81@gmail.com>
2024-03-06 00:24:30 +0300

    fix:routing-navbar

--- client/app/home/company/page.tsx
+++ client/app/home/company/page.tsx
@@ -1,20 +1,45 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import Navbar from "./Navbar";
+import { useRouter } from "next/navigation";
+
 const Company = () => {
+  const [currentPage, setCurrentPage] = useState<
+    "Dashboard" | "Jobs" | "Application" | "Profile"
+  >("Dashboard");
+  const router = useRouter() ; 
+  const navItemContent: Record<string, JSX.Element> = {
+    Dashboard: <div>Content for Dashboard</div>,
+    Jobs: <div>Content for Jobs</div>,
+    Application: <div>Content for Application</div>,
+    Profile: <div>Content for profile</div>,
+  };
+
+  const hanldeItemClick = (name: string) => {
+    setCurrentPage(name as "Dashboard" | "Jobs" | "Application" | "Profile");
+    if (name === "Jobs" || name === "Application" || name === "Profile") {
+      router.push(`/home/company?tab=${name.toLowerCase()}`);
+    }else{
+      router.push("/home/company")
+    }
+  };
+
   return (
     <>
       <div className="min-h-full">
-        <Navbar />
+        <Navbar onItemClick={hanldeItemClick} currentPage={currentPage} />
         <div className="pt-14 mt-6">
           <header className="bg-white shadow">
             <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
               <h1 className="text-2xl font-semibold tracking-tight text-gray-900">
-                Dashboard
+                {currentPage}
               </h1>
             </div>
           </header>
           <main>
-            <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8"></div>
+            <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+              {navItemContent[currentPage]}
+            </div>
           </main>
         </div>
       </div>
